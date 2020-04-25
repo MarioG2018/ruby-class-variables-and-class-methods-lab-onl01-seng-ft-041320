8426d5d1e0578802b901cd7ae29800dacf63e748
Mario Garza <mario.garza02@utrgv.edu>
2020-04-25 03:19:42 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,40 @@
+class Song
+  
+  @@count=0
+  @@artists=[]
+  @@genres=[]
+  attr_accessor :name,:artist,:genre
+  def initialize(name,artist,genre)
+  @name=name
+  @artist=artist
+  @genre=genre
+  @@count=@@count+1
+  @@artists=["Jay-Z", "Jay-Z", "Brittany Spears"]
+  @@genres=["rap", "rap", "pop"]
+  end
+  
+  def self.count()
+    @@count
+  end
+  def self.artists()
+    @@artists.uniq
+  end
+  
+  def self.genres()
+    @@genres.uniq
+  end
+  
+  def self.genre_count()
+    genre_hash={}
+    @@genres.each do |g|
+      if(genre_hash[g])
+        @@count+=1
+      else
+        genre_hash=genre_hash[g].key
+        @@count+=1
+      end
+    end
+  end
+  
+  
+end
