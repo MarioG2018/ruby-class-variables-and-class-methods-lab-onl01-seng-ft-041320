9c1839de9e897e70c1e17c3a51ad263e0f3899af
Mario Garza <mario.garza02@utrgv.edu>
2020-04-25 03:48:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,7 @@ class Song
         genre_hash[g]=1
         genre_hash[g]+=1
       else
-        genre_hash=genre_hash[g].key
+        genre_hash=genre_hash
         @@count+=1
       end
       genre_hash
